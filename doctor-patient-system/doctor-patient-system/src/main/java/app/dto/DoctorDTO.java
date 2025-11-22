60b22c50e98480a2dbfea17fa2f8438cb447c30f
D1_89388_Rushikesh <rushikeshpatil23052002@gmail.com>
2025-11-23 00:19:40 +0530

    Update DoctorDTO: add new fields and validation annotations

--- doctor-patient-system/doctor-patient-system/src/main/java/app/dto/DoctorDTO.java
+++ doctor-patient-system/doctor-patient-system/src/main/java/app/dto/DoctorDTO.java
@@ -1,10 +1,14 @@
 package app.dto;
 
+import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.ArrayList;
 import java.util.List;
 
 import app.entity.Gender;
 import app.entity.Patient;
+import app.entity.Role;
+import app.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,8 +23,11 @@ import lombok.Setter;
 @Builder
 public class DoctorDTO {
     
+	  @Schema(description = "Database ID (used only for responses)")
 	    private Long id;
+	  @Schema(description = "Email ")
 	    private String email;
+	  @Schema(description = "Name ")
 	    private String name;
 	    private Integer age;
 	    private Gender sex;
@@ -32,8 +39,9 @@ public class DoctorDTO {
 	    private String certificateUrl; // uploaded file
 	    private boolean verified = false; 
 	    private boolean active = true;
-	   
+	    private Role role=Role.DOCTOR;
 
 	 // Relations (IDs only)
+	    @Schema(description = "List of Patients IDs")
 	    private List<Long> patientIds;   // references to patients (no entity here)
 }
